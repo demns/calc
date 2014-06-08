f2fba5363b44570acc268c8c3abd10fff95028b0
MrMarDok <mrmardok@gmail.com>
2014-06-08 12:25:27 +0300

    First build
    Added operations + - * / ,

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,71 @@
-function start(){
-alert("AAA");
+var v1 = 0, v2 = 0;
+var flag = false;
+var operator = 0;
+var decimal_divider = 1;
+
+function clearscreen()
+{
+	document.getElementById("display").value = "";
+	document.getElementById("display1").value = "";
+	document.getElementById("display2").value = "";
+	v1 = 0;
+	v2 = 0;
+	decimal_divider = 1;
+}
+function input(num)
+{
+	if(decimal_divider ==1)
+	{
+		if(flag == false)
+			v1 = v1*10+num;
+		else
+			v2 = v2*10+num;
+	}
+	else
+	{
+		if(flag == false)
+			v1 = v1+num/decimal_divider;
+		else
+			v2 = v2+num/decimal_divider;
+	}
+	document.getElementById("display").value += num.toString();
+	document.getElementById("display1").value = v1;
+	document.getElementById("display2").value = v2;
 }
 
-
-function addition()
+function point()
 {
-  var a = prompt('Input first number','number1');
-  var b = prompt('Input second number','number2');
+	decimal_divider *=10;
+	document.getElementById("display").value += '.';
+}
 
-  alert(a+b);
+function operation(op)
+{
+	flag = true;
+	decimal_divider = 1;
+	document.getElementById("display").value += op;
+	operator = op;
+}
+
+function output()
+{
+	switch (operator) {
+		case '+':
+			v1 += v2;
+			break;
+		case '-':
+			v1 -= v2;
+			break;
+		case '*':
+			v1 *= v2;
+			break;
+		case '/':
+			v1 /= v2;
+			break;
+		default:
+			alert("Error! Operation was not selected!");
+	}
+	document.getElementById("display").value = v1.toString();
+	v2 = 0;
+	decimal_divider = 1;
 }
